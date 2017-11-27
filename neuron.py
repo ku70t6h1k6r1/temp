2127706964373aed0225a68520d0a568c7f12951
ku70t6h1k6r1 <uju5150@gmail.com>
2017-11-27 23:33:05 +0900

    fixed
    it can learn

--- neuron.py
+++ neuron.py
@@ -108,9 +108,6 @@ class NeuralNetwork:
             self.output_layer[iOUT].setInput(self.middle_layer[self.middle_n] * self.w_mo[self.middle_n][iOUT])
             self.output_layer[iOUT].getOutput()
 
-        #for iOUT in range (self.out_n):
-            #print self.output_layer[iOUT].output
-
         return self.output_layer
 
     def learn(self, inputData, outputData):
@@ -123,50 +120,57 @@ class NeuralNetwork:
 
         #NW出力
         nwResult = self.commit(inputData)
+        
         nwOutputData = [] #0-1
         for i in range(self.out_n):
             nwOutputData.append(nwResult[i].output)
-        
+            print nwResult[i].output
+
         #学習係数
-        k = 0.995
+        k = 0.8
 
         #BPようにメソッド作ったほうがよいのでは
         #出力層
         for iOUT in range(self.out_n):
-            self.outputBP_layer[iOUT].setInput( (1.0 -  nwOutputData[iOUT] ) *  nwOutputData[iOUT] * 2 * ( nwOutputData[iOUT] - outputData[iOUT] ) )
-            print   nwOutputData[iOUT]
+            self.outputBP_layer[iOUT].setInput( (1.0 -  nwOutputData[iOUT] ) *  nwOutputData[iOUT] * 2.0 * ( nwOutputData[iOUT] - outputData[iOUT] ) )
+
 
         #中間層
         for iMID in range(self.middle_n):
             for iOUT in range(self.out_n):
-                self.middleBP_layer[iMID].setInput(self.outputBP_layer[iOUT].input_sum)
-            self.middleBP_layer[iMID].output = self.middleBP_layer[iMID].input_sum * ( 1 - self.middle_layer[iMID].output ) * self.middle_layer[iMID].output
+                self.middleBP_layer[iMID].setInput(self.outputBP_layer[iOUT].input_sum * self.w_mo[iMID][iOUT]) 
+            self.middleBP_layer[iMID].output = self.middleBP_layer[iMID].input_sum * ( 1.0 - self.middle_layer[iMID].output ) * self.middle_layer[iMID].output
 
         #出力層→中間層
         for post in range(self.out_n):
             for pre in range(self.middle_n):
-                self.w_mo[pre][post] -= k * self.outputBP_layer[post].output *  self.middle_layer[pre].output
+                self.w_mo[pre][post] -= k * self.outputBP_layer[post].input_sum  *  self.middle_layer[pre].output
         
         #中間層→入力層
         for post in range(self.middle_n):
             for pre in range(self.in_n):
                 self.w_im[pre][post] -= k * self.middleBP_layer[post].output *  self.input_layer[pre] 
-                #self.input_layer[pre].output多分sigmoidじゃない Neuron()じゃない
+                #self.input_layer[pre].output sigmoidじゃない Neuron()じゃない
                 
         return self
         
 #テスト実行
-inputData = [0, 1]
-outputData = [1, 0]
 
 testNW = NeuralNetwork()
-NeuralNetwork.initialize(testNW, 2, 3, 2)
-NeuralNetwork.resetW(testNW)
+NeuralNetwork.initialize(testNW, 3, 10, 3)
 
-for i in range(1000000):
+for i in range(20000):
+    i1 = random.uniform(0,1)
+    i2 = random.uniform(0,1)
+    i3 = random.uniform(0,1)
+    
+    inputData = [i1, i2, i3]
+    outputData = [i1*0.5, i2*0.5, i3*0.5]
     print "###################"
-    testNW.learn(inputData, outputData)
+    NeuralNetwork.learn(testNW ,inputData, outputData)
 
-
-
+print "#TEST##################"
+NeuralNetwork.learn(testNW ,[0,0.4,0.4], [0,0.2,0.2])
+print "#TEST##################"
+NeuralNetwork.learn(testNW ,[0.4,0.6,0.3], [0.2,0.3,0.15])
 
