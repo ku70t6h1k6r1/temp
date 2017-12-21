db35924cabd1871b4155adb1632ff3751980900d
ku70t6h1k6r1 <uju5150@gmail.com>
2017-12-21 15:24:50 +0900

    dice
    dice fixed

--- dice/exec.rb
+++ dice/exec.rb
@@ -3,13 +3,13 @@ require 'matrix'
 class Dice
 	def shake(array)
 		array = ( Vector.elements(array)  / array.inject(:+) ).to_a
-		random = Random.new.rand(1e8)/1e8.to_f
+		random = Random.new.rand(1e8)/1e8.to_f		
 		
 		sumVal = 0.0
 		i =  0
 		array.each do |val|
 			sumVal = val + sumVal
-			if val  > random then
+			if sumVal  >=  random then
 				break
 			else
 				i += 1
@@ -20,8 +20,10 @@ class Dice
 	end
 end
 
-a = [1,1,2]
+a = [1.0,1.0,2.0]
 
 dice = Dice.new
-puts dice.shake(a)
 
+1000.times{
+	puts dice.shake(a)
+}
