7d2fb2ebb70e2a6ff1df97ebf40fd22378c4832a
ku70t6h1k6r1 <uju5150@gmail.com>
2018-03-07 00:25:59 +0900

    minor change createForm.py
    minor change createForm.py

--- createForm.py
+++ createForm.py
@@ -6,15 +6,17 @@ import numpy as np
 #rehC
 rehC_rythm = rythm.Create(4)
 rehC_pitch = pitch.Create(4*16)
+rev_rehC_rythm = rehC_rythm[::-1]
+rev_rehC_pitch  = rehC_pitch[::-1]
 
 
-rehC_rythm = np.r_[rehC_rythm, rehC_rythm]
-rehC_rythm = np.r_[rehC_rythm, rehC_rythm]
+#rehC_rythm = np.r_[rehC_rythm, rehC_rythm]
+#rehC_rythm = np.r_[rehC_rythm, rehC_rythm]
 
 #rehB
-rehB_rythm = rythm.Create(8)
-rehB_rythm = np.r_[rehB_rythm, rehB_rythm]
+#rehB_rythm = rythm.Create(8)
+#rehB_rythm = np.r_[rehB_rythm, rehB_rythm]
 
 #rehA
-rehA_rythm = rythm.Create(4)
-rehA_rythm = np.r_[rehA_rythm, rehA_rythm]
+#rehA_rythm = rythm.Create(4)
+#rehA_rythm = np.r_[rehA_rythm, rehA_rythm]
